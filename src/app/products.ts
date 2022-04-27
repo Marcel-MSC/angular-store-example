3aa8aa47a0c7d62b083e97ae7913a5f0f2806f97
Marcel-MSC <marcelomarcos.s.c@gmail.com>
2022-04-27 17:19:57 -0300

    added css, images and items on gitignore

--- src/app/products.ts
+++ src/app/products.ts
@@ -8,21 +8,24 @@ export interface Product {
 export const products = [
   {
     id: 1,
-    name: 'Phone XL',
+    name: 'IPhone XL',
     price: 799,
-    description: 'A large phone with one of the best screens'
+    description: 'A large phone with one of the best screens',
+    path: 'assets/img/iphonexl.png'
   },
   {
     id: 2,
-    name: 'Phone Mini',
+    name: 'IPhone Mini',
     price: 699,
-    description: 'A great phone with one of the best cameras'
+    description: 'A great phone with one of the best cameras',
+    path: 'assets/img/iphonemini.png'
   },
   {
     id: 3,
-    name: 'Phone Standard',
-    price: 299,
-    description: ''
+    name: 'IPhone 13',
+    price: 1000,
+    description: 'Best phone ever',
+    path: 'assets/img/iphone13.png'
   }
 ];
 
